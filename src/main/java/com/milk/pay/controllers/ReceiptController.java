51e0e1156d794a6b6f6194411971dea280dd3dd5
Sandrolaxx <sandro--ramos@hotmail.com>
2022-08-07 19:56:59 -0300

    Add receipt route

---
+++ src/main/java/com/milk/pay/controllers/ReceiptController.java
@@ -0,0 +1,32 @@
+package com.milk.pay.controllers;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import com.milk.pay.utils.MilkPayExceptionResponseDto;
+import com.milk.pay.utils.ReceiptUtil;
+
+/**
+ *
+ * @author SRamos
+ */
+@Path("/milkpay-api/v1/receipt")
+@Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "Receipt")
+public class ReceiptController {
+
+    @APIResponse(responseCode = "200", description = "Caso sucesso, retorna Comprovante consultado.")
+    @APIResponse(responseCode = "400", content = @Content(schema = @Schema(allOf = MilkPayExceptionResponseDto.class)))
+    @POST
+    public String consultKey(String receipt) {
+        return ReceiptUtil.handleCreate(receipt);
+    }
+
+}
--- src/main/java/com/milk/pay/utils/ReceiptUtil.java
+++ src/main/java/com/milk/pay/utils/ReceiptUtil.java
@@ -1,9 +1,18 @@
 package com.milk.pay.utils;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Base64;
+
+import javax.imageio.ImageIO;
 
 import com.milk.pay.entities.ReceiptInfo;
+import com.milk.pay.entities.enums.EnumErrorCode;
 
 /**
  *
@@ -11,15 +20,21 @@ import com.milk.pay.entities.ReceiptInfo;
  */
 public class ReceiptUtil {
 
+    private static int marginTopSize = 25;
+
     public static String createReceiptPix(ReceiptInfo receiptInfo) {
 
         var receiptLayout = replaceDefaultFields(getLayoutPixPaidReceipt(), receiptInfo);
 
-        receiptLayout = receiptLayout.replace("{inst-recebedor}", StringUtil.addBlankLeftPad(receiptInfo.getReceiverAccountBank(), 35));
-        receiptLayout = receiptLayout.replace("{agencia-recebedor}", StringUtil.addBlankLeftPad(receiptInfo.getReceiverAccountBranch(), 32));
-        receiptLayout = receiptLayout.replace("{conta-recebedor}", StringUtil.addBlankLeftPad(receiptInfo.getReceiverAccount(), 34));
+        receiptLayout = receiptLayout.replace("{inst-recebedor}",
+                StringUtil.addBlankLeftPad(receiptInfo.getReceiverAccountBank(), 35));
+        receiptLayout = receiptLayout.replace("{agencia-recebedor}",
+                StringUtil.addBlankLeftPad(receiptInfo.getReceiverAccountBranch(), 32));
+        receiptLayout = receiptLayout.replace("{conta-recebedor}",
+                StringUtil.addBlankLeftPad(receiptInfo.getReceiverAccount(), 34));
         receiptLayout = receiptLayout.replace("{end-to-end}", receiptInfo.getEndToEndId());
-        receiptLayout = receiptLayout.replace("{valor-pgto}", StringUtil.addBlankLeftPad(receiptInfo.getAmount().toString(), 30));
+        receiptLayout = receiptLayout.replace("{valor-pgto}",
+                StringUtil.addBlankLeftPad(receiptInfo.getAmount().toString(), 30));
 
         return receiptLayout;
     }
@@ -30,39 +45,57 @@ public class ReceiptUtil {
 
         if (receiptInfo.getDigitable().length() == 44) {
             receiptLayout = receiptLayout.replace("{linha-digitavel-1}", receiptInfo.getDigitable().subSequence(0, 31));
-            receiptLayout = receiptLayout.replace("{linha-digitavel-2}", receiptInfo.getDigitable().subSequence(32, 44));
-        } else if(receiptInfo.getDigitable().length() == 47) {
+            receiptLayout = receiptLayout.replace("{linha-digitavel-2}",
+                    receiptInfo.getDigitable().subSequence(32, 44));
+        } else if (receiptInfo.getDigitable().length() == 47) {
             receiptLayout = receiptLayout.replace("{linha-digitavel-1}", receiptInfo.getDigitable().subSequence(0, 31));
-            receiptLayout = receiptLayout.replace("{linha-digitavel-2}", receiptInfo.getDigitable().subSequence(32, 47));
+            receiptLayout = receiptLayout.replace("{linha-digitavel-2}",
+                    receiptInfo.getDigitable().subSequence(32, 47));
         } else {
             receiptLayout = receiptLayout.replace("{linha-digitavel-1}", receiptInfo.getDigitable().subSequence(0, 31));
-            receiptLayout = receiptLayout.replace("{linha-digitavel-2}", receiptInfo.getDigitable().subSequence(32, 48));
+            receiptLayout = receiptLayout.replace("{linha-digitavel-2}",
+                    receiptInfo.getDigitable().subSequence(32, 48));
         }
 
-        receiptLayout = receiptLayout.replace("{vcto}", StringUtil.addBlankLeftPad(DateUtil.formatDDMMYYYY(receiptInfo.getDueDate()), 21));
-        receiptLayout = receiptLayout.replace("{valor-tit}", StringUtil.addBlankLeftPad(receiptInfo.getAmount().toString(), 27));
-        receiptLayout = receiptLayout.replace("{valor-cob}", StringUtil.addBlankLeftPad(receiptInfo.getAmount().toString(), 26));
-        
+        receiptLayout = receiptLayout.replace("{vcto}",
+                StringUtil.addBlankLeftPad(DateUtil.formatDDMMYYYY(receiptInfo.getDueDate()), 21));
+        receiptLayout = receiptLayout.replace("{valor-tit}",
+                StringUtil.addBlankLeftPad(receiptInfo.getAmount().toString(), 27));
+        receiptLayout = receiptLayout.replace("{valor-cob}",
+                StringUtil.addBlankLeftPad(receiptInfo.getAmount().toString(), 26));
+
         return receiptLayout;
     }
 
     public static String replaceDefaultFields(String receiptLayout, ReceiptInfo receiptInfo) {
-        
+
         var dateTime = LocalDateTime.now();
 
-        receiptLayout = receiptLayout.replace("{protocolo}", StringUtil.addBlankLeftPad(receiptInfo.getPayment().getId().toString(), 31));
+        receiptLayout = receiptLayout.replace("{protocolo}",
+                StringUtil.addBlankLeftPad(receiptInfo.getPayment().getId().toString(), 31));
         receiptLayout = receiptLayout.replace("{data-atual}", DateUtil.formatDDMMYYYY(LocalDate.now()));
-        receiptLayout = receiptLayout.replace("{hora-atual}", StringUtil.addBlankLeftPad(dateTime.getHour() + ":" + dateTime.getMinute(),30));
-        receiptLayout = receiptLayout.replace("{tipo-movimento}", receiptInfo.getMovementCode().getValue().length() > 32 ? 
-            receiptInfo.getMovementCode().getValue().toUpperCase().substring(0, 32) : receiptInfo.getMovementCode().getValue().toUpperCase());
-        receiptLayout = receiptLayout.replace("{nome-pagador}", StringUtil.addBlankLeftPad(receiptInfo.getPayerName(), 35));
-        receiptLayout = receiptLayout.replace("{doc-pagador}", StringUtil.addBlankLeftPad(receiptInfo.getPayerDocument(), 31));
-        receiptLayout = receiptLayout.replace("{inst-pagador}", StringUtil.addBlankLeftPad(receiptInfo.getPayerAccountBank(), 35));
-        receiptLayout = receiptLayout.replace("{agencia-pagador}", StringUtil.addBlankLeftPad(receiptInfo.getPayerAccountBranch(), 32));
-        receiptLayout = receiptLayout.replace("{conta-pagador}", StringUtil.addBlankLeftPad(receiptInfo.getPayerAccount(), 34));
-        receiptLayout = receiptLayout.replace("{nome-recebedor}", StringUtil.addBlankLeftPad(receiptInfo.getReceiverName(), 35));
-        receiptLayout = receiptLayout.replace("{inst-recebedor}", StringUtil.addBlankLeftPad(receiptInfo.getReceiverAccountBank(), 35));
-        receiptLayout = receiptLayout.replace("{data-pgto}", StringUtil.addBlankLeftPad(DateUtil.formatDDMMYYYY(LocalDate.now()), 22));
+        receiptLayout = receiptLayout.replace("{hora-atual}",
+                StringUtil.addBlankLeftPad(dateTime.getHour() + ":" + dateTime.getMinute(), 30));
+        receiptLayout = receiptLayout.replace("{tipo-movimento}",
+                receiptInfo.getMovementCode().getValue().length() > 32
+                        ? receiptInfo.getMovementCode().getValue().toUpperCase().substring(0, 32)
+                        : receiptInfo.getMovementCode().getValue().toUpperCase());
+        receiptLayout = receiptLayout.replace("{nome-pagador}",
+                StringUtil.addBlankLeftPad(receiptInfo.getPayerName(), 35));
+        receiptLayout = receiptLayout.replace("{doc-pagador}",
+                StringUtil.addBlankLeftPad(receiptInfo.getPayerDocument(), 31));
+        receiptLayout = receiptLayout.replace("{inst-pagador}",
+                StringUtil.addBlankLeftPad(receiptInfo.getPayerAccountBank(), 35));
+        receiptLayout = receiptLayout.replace("{agencia-pagador}",
+                StringUtil.addBlankLeftPad(receiptInfo.getPayerAccountBranch(), 32));
+        receiptLayout = receiptLayout.replace("{conta-pagador}",
+                StringUtil.addBlankLeftPad(receiptInfo.getPayerAccount(), 34));
+        receiptLayout = receiptLayout.replace("{nome-recebedor}",
+                StringUtil.addBlankLeftPad(receiptInfo.getReceiverName(), 35));
+        receiptLayout = receiptLayout.replace("{inst-recebedor}",
+                StringUtil.addBlankLeftPad(receiptInfo.getReceiverAccountBank(), 35));
+        receiptLayout = receiptLayout.replace("{data-pgto}",
+                StringUtil.addBlankLeftPad(DateUtil.formatDDMMYYYY(LocalDate.now()), 22));
         receiptLayout = receiptLayout.replace("{autenticacao-anterior}", receiptInfo.getLastAuthentication());
 
         return receiptLayout;
@@ -146,5 +179,75 @@ public class ReceiptUtil {
         return sb.toString();
 
     }
-    
+
+    public static String handleCreate(String receipt) {
+
+        try {
+            var newImg = getBlankImage(260, 720);
+            var graphics2D = (Graphics2D) newImg.getGraphics();
+
+            graphics2D.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 12));
+            graphics2D.setColor(Color.BLACK);
+            
+            setTextCenter(graphics2D, "Teste:Sandrolax", newImg, 10);
+
+            var btArrayOutputStram = new ByteArrayOutputStream();
+            ImageIO.write(newImg, "png", btArrayOutputStram);
+
+            return new String(Base64.getEncoder().encodeToString(btArrayOutputStram.toByteArray()));
+        } catch (Exception e) {
+            throw new MilkPayException(EnumErrorCode.ERRO_SALVAR_COMPROVANTE);
+        }
+
+    }
+
+    private static Graphics2D setTextCenter(Graphics2D graphics2DImage, String string,
+            BufferedImage bgImage, int textVerticalLocation) {
+        graphics2DImage = addText(graphics2DImage, bgImage.getWidth());
+
+        return graphics2DImage;
+    }
+
+    private static BufferedImage getBlankImage(int width, int height) {
+        var newImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        var graphics2D = (Graphics2D) newImg.getGraphics();
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fillRect(0, 0, width, height);
+
+        return newImg;
+    }
+
+    private static Graphics2D addText(Graphics2D graphics2DImage, int bgImageWidth) {
+        var separator = "---------------------------------------------------------";
+
+        graphics2DImage.drawString("AKTIE TECNOLOGIA", getHorizontalSize(graphics2DImage, bgImageWidth, "AKTIE TECNOLOGIA"), marginTopSize);
+        graphics2DImage.drawString(" PROTOCOLO:", 0, getNextMarginTop());
+        graphics2DImage.drawString("XX123123", getLeftTextSize(graphics2DImage, bgImageWidth, "XX123123"), marginTopSize);
+        graphics2DImage.drawString(" TERMINAL:", 0, getNextMarginTop());
+        graphics2DImage.drawString("AKTIE-API", getLeftTextSize(graphics2DImage, bgImageWidth, "AKTIE-API"), marginTopSize);
+        graphics2DImage.drawString(separator, getHorizontalSize(graphics2DImage, bgImageWidth, separator), getNextMarginTop());
+
+        return graphics2DImage;
+
+    }
+
+    private static int getNextMarginTop() {
+        marginTopSize = marginTopSize + 20;
+
+        return marginTopSize;
+    }
+
+    private static int getHorizontalSize(Graphics2D graphics2DImage, int bgImageWidth, String string) {
+        int stringWidthLength = (int) graphics2DImage.getFontMetrics().getStringBounds(string, graphics2DImage).getWidth();
+
+        return bgImageWidth / 2 - stringWidthLength / 2;
+    }
+
+    private static int getLeftTextSize(Graphics2D graphics2DImage, int bgImageWidth, String string) {
+        int stringWidthLength = (int) graphics2DImage.getFontMetrics().getStringBounds(string, graphics2DImage).getWidth();
+
+        return bgImageWidth - stringWidthLength - 4;
+    }
+
 }
